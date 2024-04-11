6b03177455015859d39c72b6b791980eda94e4df
croisen <absolutenico32@gmail.com>
2024-04-11 22:54:21 +0800

    May the github action work? v9

--- increment_version.sh
+++ increment_version.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
 LATEST_TAG=$(git describe --abbrev=0 --tags 2>/dev/null || echo "0.0.0")
-IFS='.' read -r -a SEMVER_PARTS <<< $(echo "$LATEST_TAG" | sed "s/v//")
 
-SEMVER_PARTS[2]="$((SEMVER_PARTS[2] + 1))"
-NEW_TAG="v${SEMVER_PARTS[0]}.${SEMVER_PARTS[1]}.${SEMVER_PARTS[2]}"
+function change_ver() {
+    # $1 is PUBSPEC_VER
+    MAJOR=$(echo $LATEST_TAG | awk -F"." '{ print $1 }')
+    MAJOR=$(echo $MAJOR | sed "s/v//")
+    MINOR=$(echo $LATEST_TAG | awk -F"." '{ print $2 }')
+    PATCHES=$(echo $LATEST_TAG | awk -F"." '{ print $3 }')
 
-echo $NEW_TAG
+    PATCHES=$(($PATCHES + 1))
+    if [[ $PATCHES -ge 10 ]]; then
+        PATCHES=0
+        MINOR=$(($MINOR + 1))
+    fi
+
+    if [[ $MINOR -ge 10 ]]; then
+        MINOR=0
+        MAJOR=$(($MAJOR + 1))
+    fi
+
+    echo ${MAJOR}.${MINOR}.${PATCHES}
+}
+
+echo "v$(change_ver)"
